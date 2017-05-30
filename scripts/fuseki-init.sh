fad1df130556ed2f1e3c691f73adf2415f728fa4
Adrian Gschwend <ktk@netlabs.org>
2017-05-30 18:22:20 +0200

    Votations

--- scripts/fuseki-init.sh
+++ scripts/fuseki-init.sh
@@ -3,6 +3,7 @@
 #sleep 20
 source scripts/env.sh
 curl -u admin:cinderella --data "dbType=tdb&dbName=uster"  http://$FUSEKI_HOST:3030/$/datasets
-curl -i -X DELETE -u admin:cinderella -G http://$FUSEKI_HOST:3030/uster/data --data-urlencode graph=http://example.org/gujer
+#curl -i -X DELETE -u admin:cinderella -G http://$FUSEKI_HOST:3030/uster/data --data-urlencode graph=http://example.org/gujer
 curl -X PUT -u admin:cinderella  -T target/gujer.nt -G -H "Content-Type: application/n-triples" http://$FUSEKI_HOST:3030/uster/data --data-urlencode graph=http://example.com/gujer
+curl -X PUT -u admin:cinderella  -T target/votations.nt -G -H "Content-Type: application/n-triples" http://$FUSEKI_HOST:3030/uster/data --data-urlencode graph=http://example.com/votations
 
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,2 @@
 rdf serialize gujer.csv --output-format ntriples -o ../target/gujer.nt
+rdf serialize votations.csv --output-format ntriples -o ../target/votations.nt
