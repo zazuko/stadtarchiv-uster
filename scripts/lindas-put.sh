5dcf64cb4f5c41ff46d5f0467b770dc5c3a2abda
Adrian Gschwend <ktk@netlabs.org>
2017-06-05 12:58:03 +0200

    Create thumbnails URI

--- scripts/lindas-put.sh
+++ scripts/lindas-put.sh
@@ -3,17 +3,17 @@ curl -n \
      -X PUT \
      -H Content-Type:application/n-triples \
      -T target/gujer-clean.nt \
-     -G https://lindasdev.netrics.ch:8443/test \
+     -G https://lindasdev.netrics.ch:8443/lindas \
      --data-urlencode graph=https://linked.opendata.swiss/graph/uster
 curl -n \
      -X POST \
      -H Content-Type:application/n-triples \
      -T target/gujer-wgs84.nt \
-     -G https://lindasdev.netrics.ch:8443/test \
+     -G https://lindasdev.netrics.ch:8443/lindas \
      --data-urlencode graph=https://linked.opendata.swiss/graph/uster
 curl -n \
      -X POST \
      -H Content-Type:application/n-triples \
      -T target/votations-clean.nt \
-     -G https://lindasdev.netrics.ch:8443/test \
+     -G https://lindasdev.netrics.ch:8443/lindas \
      --data-urlencode graph=https://linked.opendata.swiss/graph/uster
